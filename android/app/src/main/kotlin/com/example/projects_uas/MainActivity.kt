fc64fb62c3c4a0cf3aafbe44882f2252df889865
Usmar Manalu <usmarmnl99@gmail,com>
2023-07-04 18:25:45 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/projects_uas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projects_uas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/data/data_source/todo_remote_data_source.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/todo_item.dart';
+import '../todo_data_source_interface.dart';
+
+class ToDoRemoteDataSource implements ToDoDataSourceInterface {
+  final toDoCollection = FirebaseFirestore.instance.collection('toDos');
+
+  @override
+  Stream<List<ToDoItem>?> fetchToDoListStream() =>
+      toDoCollection.snapshots().map<List<ToDoItem>>(
+            (event) => event.docs
+                .map((doc) => ToDoItem(
+                      id: doc.id,
+                      title: doc.data()['title'],
+                      state: doc.data()['isDone'],
+                    ))
+                .toList(),
+          );
+
+  @override
+  Future<void> onAddToDoItem(ToDoItem item) async {
+    await toDoCollection.add(
+      {
+        'title': item.title,
+        'isDone': item.state,
+      },
+    );
+  }
+
+  @override
+  Future<void> onRemoveToDoItem(String uid) async {
+    await toDoCollection.doc(uid).delete();
+  }
+
+  @override
+  Future<void> onToogleToDoItemState(String uid, bool state) async {
+    await toDoCollection.doc(uid).update({'isDone': state});
+  }
+  
+
+
+  
+}
